47255394852eaaaa7ad8bbda145f6173fd0def40
NobitaZ <doanquytb@gmail.com>
2020-09-28 18:30:25 +0700

    v106

--- main.js
+++ main.js
@@ -45,9 +45,10 @@ autoUpdater.on('error', (err) => {
   sendStatusToWindow('Error in auto-updater. ' + err);
 })
 autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  // let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  // log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  // log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  log_message = `Downloading... ${progressObj.percent}%`;
   sendStatusToWindow(log_message);
 })
 autoUpdater.on('update-downloaded', (info) => {
